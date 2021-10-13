c61f0b4e913e05bc99ba98f7296bb42eb2139892
Opal <guerre5lily@gmail.com>
2021-10-12 22:29:04 -0700

    player name function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+console.log("Hi lily!");
+console.log(10 + 10);
+console.log("Our robot's name is " + playerName);
+
+
+function fight () {
+    window.alert("The fight has begun!");
+}
+
+// fight();
