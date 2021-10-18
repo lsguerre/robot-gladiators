bbb9c1084bcb23751cb99390aeab6db4f0877a16
Opal <guerre5lily@gmail.com>
2021-10-17 22:03:42 -0700

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,12 +2,13 @@ var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
-console.log(playerName, playerAttack, playerHealth,);
+
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"]
-console.log(enemyNames.length);
 var enemyHealth = 50;
 var enemyAttack = 12;
+
+
 
 var fight = function(enemyName) {
     while (playerHealth > 0 && enemyHealth > 0) {
@@ -68,10 +69,15 @@ var fight = function(enemyName) {
     
 
 
-
+var startGame = function() {
 for(var i = 0; i < enemyNames.length; i++) {
+    //reset player stats
+    playerHealth = 100
+    playerAttack = 10
+    playerMoney = 10
+
     if (playerHealth > 0) {
-        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1) )   
+        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) )   
         var pickedEnemyName = enemyNames[i]
         enemyHealth = 50
         fight(pickedEnemyName)
@@ -80,4 +86,30 @@ for(var i = 0; i < enemyNames.length; i++) {
         window.alert("You have lost your robot in battle! Game Over!")
         break
     }
+
+ }  
+   //play again
+
+    //after loop ends player either out of health or enemies to fight so run endgame function
+    endGame()
 }
+
+var endGame = function() {
+    window.alert("The game has now ended. Let's see how you did!")
+    // if player is still alive they win!
+    if (playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".")
+}
+    else {
+        window.alert("You've lost your robot in battle.")
+    }
+    var playAgainConfirm = window.confirm("Would you like to play again?")
+    if (playAgainConfirm) {
+        //restart game
+        startGame()
+    } else {
+        window.alert("Thank you for playing Robot Gladiators Come back soon!")
+    }
+}
+
+startGame()
